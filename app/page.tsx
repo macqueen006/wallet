f83b99f4ddc2f4cb7f3363e2a2a74ed934d3b193
godwin <65357127+macqueen006@users.noreply.github.com>
2024-09-21 20:09:41 +0100

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,120 @@
+"use client"
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+import { MdArrowDropUp } from "react-icons/md";
+import { IoPaperPlaneOutline } from "react-icons/io5";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { IoIosMenu } from "react-icons/io";
+import { FaX } from "react-icons/fa6";
+import { BsFillMenuButtonWideFill } from "react-icons/bs";
+import { CgListTree } from "react-icons/cg";
+import { IoMdHome } from "react-icons/io";
+import { BsViewList } from "react-icons/bs";
+import Header from "@/components/Header";
+import Aside from "@/components/Aside";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="h-screen">
+      <Header />
+      <main className="h-full">
+        <div className="md:flex w-full mx-auto h-full">
+          <div className="w-20 bg-light-grey h-full md:block hidden p-4">
+            <button className="text-2xl font-bold italic">WT</button>
+            <div className="flex flex-col gap-2 mt-8">
+              <Link href="/" className="w-10 h-10 rounded-lg bg-white flex items-center justify-center hover:bg-slate-800 group"><IoMdHome className="inline-block group-hover:text-white" /></Link>
+              <Link href="/wallets" className="rounded-lg w-10 h-10 bg-white flex items-center justify-center hover:bg-slate-800 group"><BsViewList className="inline-block group-hover:text-white" /></Link>
+            </div>
+          </div>
+          <section className="w-full md:w-2/3">
+            <div className="p-4">
+              <div className="p-4 lg:p-8 bg-yellow-200 text-center rounded-lg">
+                <p className="text-sm">My wallet</p>
+                <h2 className="text-3xl font-bold pt-6">$128,192</h2>
+                <span className="text-xs font-bold text-slate-700">+$15,258</span>
+              </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+              <div className="mt-8">
+                <ul className="space-y-2">
+                  <li>
+                    <div className="flex justify-between items-center border border-slate-200 py-2 px-4 rounded-lg">
+                      <div className="flex flex-col">
+                        <span>Bitcoin</span>
+                        <span className="text-xs text-slate-500">BTC</span>
+                      </div>
+                      <div className="">
+                        <span>$5,230.12</span>
+                        <span className="text-green-500 text-xs"><MdArrowDropUp className="inline-block ml-2" />2.31%</span>
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="flex justify-between items-center border border-slate-200 py-2 px-4 rounded-lg">
+                      <div className="flex flex-col">
+                        <span>Bitcoin</span>
+                        <span className="text-xs text-slate-500">BTC</span>
+                      </div>
+                      <div className="">
+                        <span>$5,230.12</span>
+                        <span className="text-green-500 text-xs"><MdArrowDropUp className="inline-block ml-2" />2.31%</span>
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="flex justify-between items-center border border-slate-200 py-2 px-4 rounded-lg">
+                      <div className="flex flex-col">
+                        <span>Bitcoin</span>
+                        <span className="text-xs text-slate-500">BTC</span>
+                      </div>
+                      <div className="">
+                        <span>$5,230.12</span>
+                        <span className="text-green-500 text-xs"><MdArrowDropUp className="inline-block ml-2" />2.31%</span>
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="flex justify-between items-center border border-slate-200 py-2 px-4 rounded-lg">
+                      <div className="flex flex-col">
+                        <span>Bitcoin</span>
+                        <span className="text-xs text-slate-500">BTC</span>
+                      </div>
+                      <div className="">
+                        <span>$5,230.12</span>
+                        <span className="text-green-500 text-xs"><MdArrowDropUp className="inline-block ml-2" />2.31%</span>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+                <Link href="/wallets" className="text-sm mt-4 text-blue-400 inline-block">View all Wallets</Link>
+              </div>
+            </div>
+            {/* wallet amount */}
+          </section>
+          <Aside />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
